c5271c731bdfd647a3d3ebd07fd33164f8722c3d
Anish Shukla <shuklaanish040@gmail.com>
2024-09-22 11:15:04 +0530

    first commit

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
-const port = 5000;
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const connectDB = require("./config/db");
+
+connectDB();
 
 const app = express();
 
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/", (req, res) => {
-  res.json({ Message: "Welcome to the RandomIdeas API" });
+  res.json({ message: "Welcome to the RandomIdeas API" });
 });
 
 const ideasRouter = require("./routes/ideas");
