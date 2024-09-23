a78926a63d4c207b80a2a976a793a693a6779442
Anish Shukla <shuklaanish040@gmail.com>
2024-09-23 19:15:50 +0530

    first commit

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,11 @@
 import "@fortawesome/fontawesome-free/css/all.css";
 import IdeaForm from "./components/IdeaForm";
+import IdeaList from "./components/IdeaList";
 import Modal from "./components/Modal";
 import "./css/style.css";
 
 const modal = new Modal();
 const ideaForm = new IdeaForm();
 ideaForm.render();
+const ideaList = new IdeaList();
+ideaList.render();
